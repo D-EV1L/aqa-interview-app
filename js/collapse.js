deee4371a31d356ad99de29a56b08ef636475a8a
Volodymyr Vdovikovskyi <we5t.d1rew0lf.ua@gmail.com>
2023-07-18 16:23:37 +0300

    fixed resizing of container

--- js/collapse.js
+++ js/collapse.js
@@ -1,26 +1,16 @@
 function collapseSection(element) {
-    var sectionHeight = element.scrollHeight;
-
-    var elementTransition = element.style.transition;
-    element.style.transition = '';
-
-    requestAnimationFrame(function () {
-        element.style.height = sectionHeight + 'px';
-        element.style.transition = elementTransition;
-
-        requestAnimationFrame(function () {
-            element.style.height = 0 + 'px';
-        });
-    });
-
+    element.style.height = 0 + 'px';
     element.setAttribute('data-collapsed', 'true');
 }
 
 function expandSection(element) {
     var sectionHeight = element.scrollHeight;
     element.style.height = sectionHeight + 'px';
-
     element.setAttribute('data-collapsed', 'false');
+
+    element.addEventListener("transitionend", function () {
+        element.style.height = 'auto';
+    });
 }
 
 document.addEventListener('click', function (e) {
